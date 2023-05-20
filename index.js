c037d56a8a48db2e4c6a39f1f07fe40aef3ec80a
asad2002web <asad2002web@gmail.com>
2023-05-20 14:34:15 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -3,10 +3,38 @@ const cors = require('cors');
 const port = process.env.PORT || 4000;
 const app = express();
 
+
+// ToyCraze
+// ToyCraze1234 
 // middleware
 app.use(cors());
 
+// MongoDB code 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ToyCraze:ToyCraze1234@cluster0.ehhcnw6.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -14,5 +42,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`ToyCraze Server 100 is running on port ${port}`)
+    console.log(`ToyCraze Server is running on port ABC ${port}`)
 })
