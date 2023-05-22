4aa72503ea3a090a66f7d9f494d9f41e7af8dd17
asad2002web <asad2002web@gmail.com>
2023-05-22 23:29:58 +0600

    mongodb update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
 
-
-// ToyCraze
-// ToyCraze1234 
 // middleware
 app.use(cors());
+app.use(express.json());
 
-// MongoDB code 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ToyCraze:ToyCraze1234@cluster0.ehhcnw6.mongodb.net/?retryWrites=true&w=majority";
+// MongoDB code
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehhcnw6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,16 +18,23 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    // Server route API
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,11 +42,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('ToyCraze is running')
-})
+app.get("/", (req, res) => {
+  res.send("ToyCraze is running");
+});
 
 app.listen(port, () => {
-    console.log(`ToyCraze Server is running on port ABC ${port}`)
-})
+  console.log(`ToyCraze Server is running on port ABC ${port}`);
+});
