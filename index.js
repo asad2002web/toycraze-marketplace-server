63a58f586e728bc4076d044a9efde1e177664957
asad2002web <asad2002web@gmail.com>
2023-05-22 23:40:02 +0600

    mongodb with env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-// MongoDB code
+// MongoDB code //
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehhcnw6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +26,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    const myToyCollection = client.db('crazeToyDB').collection('myToy');
     // Server route API
 
 
